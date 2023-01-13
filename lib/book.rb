3257b3adafd6a17e48a64eee547a482145539aa5
Harinder Singh <harmiglany@yahoo.com>
2023-01-13 12:53:30 -0800

    commit tree c9882fa58d4340cc200926c96a7dfde059d29e0a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
+
+new_book = Book.new("my new book")
+james_peach = Book.new("James and the Giant Peach")
+james_peach.title = "James Peach"
+james_peach.title = ("James Peach")
